78467629c0b2b868dea840dc95aacf3500e33315
Kizito007 <nwakakelechi49@gmail.com>
2024-10-15 14:01:50 +0100

    feat project structure setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FacesModule } from './faces/faces.module';
+import { CommerceAdminModule } from './commerce-admin/commerce-admin.module';
+import { EmployeeAdminModule } from './employee-admin/employee-admin.module';
+import { SchoolAdminModule } from './school-admin/school-admin.module';
 
 @Module({
-  imports: [],
+  imports: [FacesModule, CommerceAdminModule, EmployeeAdminModule, SchoolAdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
