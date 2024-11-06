97b033412ba5f3bbd1ac249f1cfc41d714405e94
Kizito007 <nwakakelechi49gmail.com>
2024-11-06 02:06:40 +0100

    feat: commerce cyb structure setup

--- src/commerce-admin/commerce-admin.module.ts
+++ src/commerce-admin/commerce-admin.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AdminModule } from './admin/admin.module';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 
-@Module({})
+@Module({
+  imports: [AdminModule, UsersModule, ProductsModule, OrdersModule]
+})
 export class CommerceAdminModule {}
